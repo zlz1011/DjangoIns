ebc6d097374b6d0ffc6f81da78ed48fe548e1351
zlz1996 <zgcqlingzz@gmail.com>
2020-03-11 15:57:42 +0800

    Post and image

--- Insta/models.py
+++ Insta/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+#第三方库
+#django model type
+from imagekit.models import ProcessedImageField
 # Create your models here.
+#models.Model的子类
+class Post(models.Model):
+    title = models.TextField(blank=True,null=True)
+    image = ProcessedImageField(
+        upload_to='static/images/posts',
+        format= 'JPEG',
+        options={'quality':100},
+        blank=True,
+        null=True
+    )
--- Instar/settings.py
+++ Instar/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #自己创建的app:
     'Insta',
+    #第三方app:
+    'imagekit'
 ]
 
 MIDDLEWARE = [
